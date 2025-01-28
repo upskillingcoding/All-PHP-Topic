9fd98a4409f5fd7e5f6c4cbc8d277200a3ef1466
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-01-28 22:03:43 +0530

    program3

---
+++ programming-question/program3.php
@@ -0,0 +1,10 @@
+<?php
+$prog = "Create a PHP program to add two Numbers";
+echo $prog . "<br>" . "<hr>";
+
+$num1 = 10;
+$num2 = 15;
+$sum = $num1 + $num2;
+echo "The Add of Two Number is ". $sum;
+
+?>
