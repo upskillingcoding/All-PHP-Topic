0b6a996a0643a6e3ddf2c4662417ef6344cb32bd
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-01-28 20:05:23 +0530

    question

--- interviewquestion/basic.php
+++ interviewquestion/basic.php
@@ -11,9 +11,41 @@
         <h2>Q1. What is PHP ?</h2>
         <li>PHP is a server-side scripting language used for web development and can be embedded into HTML.</li>
     </div>
+
     <div class="div-main">
         <h2>Q2. How do you define a variable in PHP?</h2>
         <li>Variables in PHP are defined using a dollar sign ($) followed by the variable name. Example: $var = 10;.</li>
     </div>
+
+    <div class="div-main">
+        <h2>Q3. What are the superglobals in PHP ?</h2>
+        <li>Superglobals are built-in arrays in PHP such as $_GET, $_POST, $_REQUEST, $_SESSION, $_COOKIES, etc that are accessible from any part of the script</li>
+    </div>
+
+    <div class="div-main">
+        <h2>Q4. What is the difference between $_GET and $_POST ?</h2>
+        <li> $_GET is used to collect from data after submitting an HTML from using the GET method, and the data is visible in URL</li>
+        <li>$_POST is used for the POST method, where the data is sent in the HTTP request body, not visible in URLs.</li>
+    </div>
+
+    <div class="div-main">
+        <h2>Q5. How do you include a file in PHP ?</h2>
+        <li>You can include a file using include('filename.php') or require('filename.php')</li>
+    </div>
+
+    <div class="div-main">
+        <h2>Q6. What is the difference between include and require in PHP </h2>
+        <li>Include :- include will generate a warming if the file is not found but contine excute the script.</li>
+        <li>Required :- require will generate a fatel error and stop the execution if file is not found.</li>
+    </div>
+
+
+
+    <div class="div-main">
+        <h2>Q4. </h2>
+        <li></li>
+    </div>
+
+
 </body>
 </html>
