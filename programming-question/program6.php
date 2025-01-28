76ef3629a38992ac834137fa54b4594151b5a460
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-01-28 22:34:47 +0530

    program6

---
+++ programming-question/program6.php
@@ -0,0 +1,23 @@
+<?php
+
+$prog = "Write a PHP program to find the largest number in an array using prefine function";
+echo $prog . "<br>" . "<hr>";
+
+$num = [10, 51, 20, 85, 90, 10, 30];
+$largestNum = max($num);
+echo "The largest Number of an Array is " . $largestNum . "<hr> <br>";
+
+
+$prog1 = "without using predefined Function";
+echo $prog1 . "<br>" . "<hr>";
+
+$num1 = [10, 51, 20, 85, 90, 10, 30];
+
+
+
+
+
+
+
+
+?>
