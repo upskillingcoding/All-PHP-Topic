908f776dd66d6a1374ad4161a28e4fcdb9f7e5e1
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-01-28 19:48:24 +0530

    Q1 AND Q2

--- interviewquestion/basic.php
+++ interviewquestion/basic.php
@@ -1,6 +1,19 @@
-<?php
-
-echo "basic";
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>200 question in PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="div-main">
+        <h2>Q1. What is PHP ?</h2>
+        <li>PHP is a server-side scripting language used for web development and can be embedded into HTML.</li>
+    </div>
+    <div class="div-main">
+        <h2>Q2. How do you define a variable in PHP?</h2>
+        <li>Variables in PHP are defined using a dollar sign ($) followed by the variable name. Example: $var = 10;.</li>
+    </div>
+</body>
+</html>
