c7705b57e7a5ec364479326776fe3034a9aadf72
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-01-29 22:38:21 +0530

    concate the string

--- programming-question/program18.php
+++ programming-question/program18.php
@@ -3,4 +3,9 @@
 $prog = "Create a PHP program to concatenate two strings.";
 echo $prog . "<hr> <br>";
 
+$str1 = "Hello";
+$str2 = "World";
+
+$fullString = $str1 . " ". $str2;
+echo $fullString;
 ?>
