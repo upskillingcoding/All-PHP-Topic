9e32e79d114534d5f2623cc05a61f120a6fb3bdd
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-01-29 09:39:16 +0530

    program7

---
+++ programming-question/program7.php
@@ -0,0 +1,19 @@
+<?php
+$prog = "Write a PHP program to reverse a string ";
+echo $prog . "<br>". "<hr>";
+
+echo " using predefined Function";
+
+$str = "My Name Is Ritik Gupta";
+$revStr = strrev($str);
+echo "This String is $str and Reverse String is : -". $revStr;
+
+echo "<br><br> WithOut Using Predefined Function <hr>";
+
+$str1 = "My Name Is Ritik Baniya";
+for($i=0; $i<strlen($i) -1; $i--){
+    
+}
+
+
+?>
