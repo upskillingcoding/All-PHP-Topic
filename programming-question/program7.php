a6ba19baa2570d61868841b883c2d63077d7ed29
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-01-29 09:55:41 +0530

    program7

--- programming-question/program7.php
+++ programming-question/program7.php
@@ -14,10 +14,18 @@
 
 
     $str1 = "My Name Is Ritik Baniya";
-    $reverseStr = "";
-    for($i=strlen($str1) -1; $i >= 0; $i--){
-        $reverseStr .= $str1[$i];
+    $reverseStr = "";       //initialize an empty string to hold the reverse string
+    for($i=strlen($str1) -1; $i >= 0; $i--){        // looping through string in reverse order
+        $reverseStr .= $str1[$i];                       // append each character through the loop
     }
     echo "Reverse String :- ". $reverseStr;
+
+
+    echo "<br><hr>";
+    $str2 = "RITIK GUPTA";
+    $strlen = strlen($str2);
+    for($i=($strlen-1); $i>=0; $i--){
+        echo $str2[$i];
+    }
 
 ?>
---
+++ programming-question/program8.php
@@ -0,0 +1,11 @@
+<?php
+
+$prog = "Write a PHP program that check if a string is a palindrome or not ";
+echo $prog . "<hr> <br>";
+
+
+
+
+
+
+?>
